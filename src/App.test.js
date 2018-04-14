5afa9e3b98b2bd5a8be500aad720cd6507db5b33
Benjamin Huang <bhuang@rms.com>
2018-04-13 17:04:26 -0700

    can work

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
+
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import { App } from './App';
+import { initialState } from './reducers/';
 
 it('App renders without crashing', () => {
-  const component = shallow(<App />);
+  const mockFunction = jest.fn();
+
+  const component = shallow(
+    <App
+      state={initialState}
+      submitTodo={mockFunction}
+      todos={[]}
+      deleteTodo={mockFunction}
+      undeleteTodo={mockFunction}
+      inputChanged={mockFunction}
+      disableAddTodo
+      disableUndelete
+    />,
+  );
+
   expect(component.exists()).toEqual(true);
 });
